53e246d9387282577f386bf5e8fbaae05dbeea06
RuchitaAb09 <rabhang09@gmail.com>
2021-10-01 18:42:31 +0530

    journal page

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,18 @@ import Landingpage from './components/Landingpage';
 import Login from './components/Login';
 import Heading from './components/Heading'
 import Register from './components/Register';
+import Journal from './components/Journal';
 
 const App = () => {
   return (
     <>
       <Heading />
-      <Navbar />
-      <div className='container'>
-        <div><Landingpage />
-          <Login /></div>
-      </div>
+      {/* <div className='container'> */}
+        {/*<div><Landingpage />
+          <Login /></div>*/}
+      {/* </div> */}
+      <div><Navbar />
+      <Journal /></div>
     </>
   )
 }
