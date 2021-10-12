4ec7101d7387651f4f474a8b15d3d4618a06a29d
Yash <hunt.yash@gmail.com>
2021-10-12 17:39:45 +0530

    added Routing to the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,54 @@
 
 import './App.css';
-import { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Landingpage from './components/Landingpage';
 import Login from './components/Login';
 import Heading from './components/Heading'
 import Register from './components/Register';
 import Journal from './components/Journal';
-import TodoList from './components/ToDoList';
 import AddToDoList from './components/AddToDoList';
+import Layout from './pages/Layout';
+import axios from 'axios'
+import { useEffect } from 'react';
+const App = () => {
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_BaseURL}/journal/getall/6154a1b2496c5372c3a7a0fb`).then(response => console.log(response))
 
-  const App = () => {
+  }, [])
   return (
-    <>
-      <Heading />
-      {/* <div className='container'> */}
-        {/*<div><Landingpage />
-          <Login /></div>*/}
-      {/* </div> */}
-      <div><Navbar />
-      <AddToDoList/></div>
-    </>
+    <Router>
+      <Switch>
+        <Route path="/test">
+          <h1>TESTING THE ROUTER</h1>
+        </Route>
+        <Route exact path="/">
+          <Heading />
+          <Layout>
+            <Landingpage />
+          </Layout>
+        </Route>
+        <Route path="/todo">
+          <Heading />
+          <Layout>
+            <AddToDoList />
+          </Layout>
+        </Route>
+        <Route path="/journal">
+          <Heading />
+          <Layout>
+            <Journal />
+          </Layout>
+        </Route>
+        {/* <Route path="*">
+          404 not found
+        </Route> */}
+      </Switch>
+    </Router>
   )
 }
 
