5bb89f3bea401f6836da1ebd6f564c7e131dc05b
Yash <hunt.yash@gmail.com>
2021-10-18 21:19:58 +0530

    invalid date error fixed

--- client/src/components/Journal.js
+++ client/src/components/Journal.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Journal = ({ data, title, date, id, deleteFunc, setUpdate, setTitle, setData, setjournalId }) => {
-    let parsedDate = new Date(date?date:'')
+    let parsedDate = date ? new Date(date) : new Date()
     let datestring = parsedDate.toLocaleDateString()
 
     const handleDelete = (e) => {
